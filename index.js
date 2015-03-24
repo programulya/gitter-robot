2437adbd290777bc0aeaec808f52d212a112df76
Julia Savinkova <julsavinkova@gmail.com>
2015-03-24 19:42:27 +0200

    Add math.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 var Gitter = require('node-gitter');
+var math = require('mathjs');
 var gitter = new Gitter('f5e5b956482e38436dd8d9870967e3d654198d2b');
 
 gitter.currentUser()
@@ -10,21 +11,28 @@ gitter.currentUser()
         console.log('You are logged in as:', user.username);
     });
 
-gitter.rooms.join('programulya/Aleph.js', function(err, room) {
-    if (err) {
-        console.log('Error during to joining the room: ', err);
+gitter.rooms.join('programulya/Aleph.js', function(error, room) {
+    if (error) {
+        console.log('Error during to joining the room: ', error);
         return;
     }
 
     console.log('Room: ', room.name);
 
-    //room.send('Test!');
+    room.send('calc 12 / (2.3 + 0.7)');
 
     var events = room.listen();
 
     events.on('message', function(message) {
-        //TODO math expressions
-        console.log('New message:', message.text);
+        console.log(message.text.indexOf('calc'));
+
+        if (message.text.indexOf('calc ') >= 0)
+        {
+            //math.eval(message.text.indexOf(5,));
+            console.log('Calc message:', message.text);
+        } else {
+            console.log('No calc message:');
+        }
     });
 });
 
